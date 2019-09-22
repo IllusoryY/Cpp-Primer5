edeec8040c056d74d9e6514a9caccf3978cc4e18
IllusoryY <cythis@outlook.com>
2019-09-22 17:02:29 +0800

    for envirment

---
+++ ch11/ex11_7.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+
+using std::cin; using std::count; using std::endl;
+using std::map;
+using std::vector;
+using std::string;
+using Families = map<string, vector<string>>;
+
+auto make_families()
+{
+	Families families;
+	for(string ln; cout << "Last name:\n", cin >> ln && ln != "@q";)
+		for(string cn; cout << "Childrem name:\n", cin >> cn && cn != "@q")
+			families[ln].push_back(cn);
+	return families;
+}
+
+auto print(Families const& f)
+{
+	for(auto elem : f)
+	{
+		cout << elem.first << " : ";
+		for(auto i : elem.second)
+			cout << i << " ";
+		cout << endl;
+	}
+}
+
+int main()
+{
+	auto f = make_families();
+	print(f);
+	
+	return 0;
+}
